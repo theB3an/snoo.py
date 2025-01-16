a3ed083e65c5173d528fe9017093cc861513f1da
theB3an <theb3an@proton.me>
2025-01-16 15:29:33 -0500

    bug fix for maq and da

--- snoopy/lib/functions/get_DAs.py
+++ snoopy/lib/functions/get_DAs.py
@@ -1,11 +1,20 @@
 def get_domain_administrators(ldap_connection, logger, DN):
     logger.open("domain_administrators.txt")
     try:
-        resp = ldap_connection.search(searchFilter=f"(&(objectCategory=user)(|(memberOf=cn=Domain Admins,ou=Groups,{DN})(memberOf=cn=Enterprise Admins,ou=Groups,{DN})(memberOf=cn=Administrators,ou=Groups,{DN})))", attributes=['sAMAccountName'])
+        resp = ldap_connection.search(searchFilter=f"(&(objectCategory=group)(|(sAMAccountName=Domain Admins)(sAMAccountName=Enterprise Admins)(sAMAccountName=Administrators)))", attributes=['distinguishedName'])
         count = 0
+        paths = []
         for entry in resp:
             if 'attributes' in entry:
-                user = entry['attributes'][0]["vals"][0]
+                paths.append(str(entry['attributes'][0]["vals"][0]))
+
+        conditions = [f"(memberOf={group_dn})" for group_dn in paths]
+        query = f"(&(objectCategory=user)(|{''.join(conditions)}))"
+        
+        resp2 = ldap_connection.search(searchFilter=query, attributes=['sAMAccountName'])
+        for entry2 in resp2:
+            if 'attributes' in entry2:
+                user = entry2['attributes'][0]["vals"][0]
 
                 logger.log(f"  - {user}")
                 count=count+1
--- snoopy/lib/functions/machine_account_quota.py
+++ snoopy/lib/functions/machine_account_quota.py
@@ -1,13 +1,13 @@
 def get_machine_account_quota(ldap_connection, logger):
     logger.open("machine_account_quota.txt")
     try:
-        resp = ldap_connection.search(searchFilter="(objectClass=*)", attributes=['ms-DS-MachineAccountQuota'])
+        resp = ldap_connection.search(searchFilter="(objectClass=domain)", attributes=['ms-DS-MachineAccountQuota'])
 
         quota = resp[0]['attributes'][0]["vals"][0]
-        print(f"[+] msDS-MachineAccountQuota: {quota}")
+        print(f"[+] ms-DS-MachineAccountQuota: {quota}")
         logger.log(f"msDS-MachineAccountQuota: {quota}")
 
     except Exception as e:
-        print(f"[!] Error retrieving msDS-MachineAccountQuota: {e}")
+        print(f"[!] Error retrieving ms-DS-MachineAccountQuota: {e}")
 
     logger.close()
